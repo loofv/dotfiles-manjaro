ddd89993ea601e90b824dccc17e10cdd955abca2
Love <loofas@gmail.com>
2021-12-28 00:03:50 +0100

    Fixes for polybar:
      * Fixed icons on small screen'
      * Fixed hidden bar overlapping behind

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,21 +2,23 @@
 
 # Terminate already running bar instances
 killall -q polybar
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
 # Launch bar1 and bar2
 #echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-echo "---" | tee -a /tmp/example.log
-polybar example 2>&1 | tee -a /tmp/example.log & disown
+echo "---" | tee -a /tmp/bar1.log
+polybar bar1 2>&1 | tee -a /tmp/bar1.log & disown
 # polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload bar1 &
   done
 else
-  polybar --reload example &
+  polybar --reload bar1 &
 fi
 
 echo "Bars launched..."
